1a0fc7ccde95e2eeba630ad6f632e93274a40778
AlphaFoxz <841958335@qq.com>
2024-12-12 00:07:42 +0800

    tests

--- lib/__tests__/agg.test.ts
+++ lib/__tests__/agg.test.ts
@@ -3,13 +3,15 @@ import { createDomainDesigner } from '..'
 
 it('agg 定义', () => {
   const d = createDomainDesigner()
-  const agg1 = d.agg('agg1', ['field1', d.info.field.id('field2')])
+  const agg1 = d.agg('agg1', ['field1', d.info.field.id('field2'), d.info.func('field3', ['a', 'b'])])
   expect(agg1._attributes.name).toBe('agg1')
   expect(agg1.inner.field1).not.toBeUndefined()
   expect(agg1.inner.field2).not.toBeUndefined()
+  expect(agg1.inner.field3).not.toBeUndefined()
 
-  const agg2 = d.agg('agg2', () => ['field1', d.info.field.id('field2')])
+  const agg2 = d.agg('agg2', () => ['field1', d.info.field.id('field2'), d.info.doc('field3')])
   expect(agg2._attributes.name).toBe('agg2')
   expect(agg2.inner.field1).not.toBeUndefined()
   expect(agg2.inner.field2).not.toBeUndefined()
+  expect(agg2.inner.field3).not.toBeUndefined()
 })
