5a81b9c880200f5cbfd6903d0b6ab0ab817081b9
AlphaFoxz <841958335@qq.com>
2025-03-25 18:46:46 +0800

    rollback

--- lib/check/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { it, expect } from 'vitest'
-import { loadWasm } from '../index'
-
-it('loadWasm', async () => {
-  const api = await loadWasm()
-  expect(api.matchString).instanceOf(Function)
-  expect(api.matchTable).instanceOf(Function)
-})
